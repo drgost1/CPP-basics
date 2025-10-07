fdd06adc439fa7c8091d2b6b3443f4d7c240001b
NAFIS IQBAL (ZORO) <dr.gost5@gmail.com>
2025-10-07 15:56:16 +0600

    1st

---
+++ helloworld.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    string name;
+    int num;
+    cout << "I love Programming"<<endl;
+    cout << "I love Programming"<<endl;
+    cout << "Enter Your Name Please :";
+    // cin>>name;
+    // cout<<"access denied for "<<name;
+    cout<<"\n size of int :"<<sizeof(int);
+    cout<<"\n size of bool :"<<sizeof(bool);
+    cout<<"\n size of char :"<<sizeof(char);
+    cout<<"\n size of string :"<<sizeof(string);
+    cout<<"\n size of float :"<<sizeof(float);
+    cout<<"\n size of double :"<<sizeof(double);
+    cout<<"\n size of long :"<<sizeof(long);
+    cout<<"\n size of long long :"<<sizeof(long long);
+    return 0;
+}
